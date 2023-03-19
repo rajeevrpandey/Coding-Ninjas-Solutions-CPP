95711d996320471adac891a7a83519b5538e2601
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:53:19 +0530

    Update and rename Fibonacci Number.cpp to Lecture 7: Functions/Fibonacci Number.cpp

--- Lecture 7: Functions/Fibonacci Number.cpp
+++ Lecture 7: Functions/Fibonacci Number.cpp
@@ -1,3 +1,25 @@
+/*
+Given a number N, figure out if it is a member of fibonacci series or not. Return true if the number is member of fibonacci series else false.
+Fibonacci Series is defined by the recurrence
+    F(n) = F(n-1) + F(n-2)
+where F(0) = 0 and F(1) = 1
+
+
+Input Format :
+Integer N
+Output Format :
+true or false
+Constraints :
+0 <= n <= 10^4
+Sample Input 1 :
+5
+Sample Output 1 :
+true
+Sample Input 2 :
+14
+Sample Output 2 :
+false    
+*/
 bool checkMember(int n){
 
   /* Don't write main().
