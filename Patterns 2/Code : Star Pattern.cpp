cb68aac9b5691f7fd976a7160a74b01abebd4d5d
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 08:06:23 +0530

    Update and rename Code : Star Pattern.cpp to Patterns 2/Code : Star Pattern.cpp

--- Patterns 2/Code : Star Pattern.cpp
+++ Patterns 2/Code : Star Pattern.cpp
@@ -1,3 +1,32 @@
+/*
+Print the following pattern
+Pattern for N = 4
+
+    *
+   *** 
+  *****
+ *******
+ 
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Constraints :
+0 <= N <= 50
+Sample Input 1 :
+3
+Sample Output 1 :
+   *
+  *** 
+ *****
+Sample Input 2 :
+4
+Sample Output 2 :
+    *
+   *** 
+  *****
+ *******
+*/
 #include<iostream>
 using namespace std;
 
