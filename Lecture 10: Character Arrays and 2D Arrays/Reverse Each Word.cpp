5384abca4d32a15abe84eb05ae8df573d2540203
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 14:59:41 +0530

    Update and rename Reverse Each Word.cpp to Lecture 10: Character Arrays and 2D Arrays/Reverse Each Word.cpp

---
+++ Lecture 10: Character Arrays and 2D Arrays/Reverse Each Word.cpp
@@ -0,0 +1,52 @@
+/*
+Aadil has been provided with a sentence in the form of a string as a function parameter. The task is to implement a function so as to print the sentence such that each word in the sentence is reversed.
+Example:
+Input Sentence: "Hello, I am Aadil!"
+The expected output will print, ",olleH I ma !lidaA".
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. The input string represents the sentence given to Aadil.
+Output Format:
+The only line of output prints the sentence(string) such that each word in the sentence is reversed. 
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+Welcome to Coding Ninjas
+Sample Output 1:
+emocleW ot gnidoC sajniN
+Sample Input 2:
+Always indent your code
+Sample Output 2:
+syawlA tnedni ruoy edoc
+*/
+
+#include <bits/stdc++.h>
+void reverseEachWord(char input[]) {
+    int len = 0;
+    while(input[len] != '\0'){ 
+        len++;
+    }
+    int start, end, i, j=0;
+    for(int i=0;i<len;i++){
+        start = i;
+        while(input[j]!='\0'&&input[j]!=' ')
+            j++;
+        end = j-1;
+        while(start<end){
+            //swapping the characters at start and end
+            char temp = input[start];
+            input[start] = input[end];
+            input[end] = temp;
+            start++;
+            end--; 
+        }
+        if(j >= len)
+            break;
+        i=j;
+        j++;
+    }
+     
+    
+}
