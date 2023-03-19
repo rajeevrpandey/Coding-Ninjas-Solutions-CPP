0c8ea422b0cdb26f5ac8fc3660303e33ab5a1d26
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:17:03 +0530

    Update and rename Multiplication (Recursive).cpp to Lecture 3: Recursion 1/Multiplication (Recursive).cpp

---
+++ Lecture 3: Recursion 1/Multiplication (Recursive).cpp
@@ -0,0 +1,29 @@
+/*
+Given two integers M & N, calculate and return their multiplication using recursion. You can only use subtraction and addition for your calculation. No other operators are allowed.
+Input format :
+Line 1 : Integer M
+Line 2 : Integer N
+Output format :
+M x N
+Constraints :
+0 <= M <= 1000
+0 <= N <= 1000
+Sample Input 1 :
+3 
+5
+Sample Output 1 :
+15
+Sample Input 2 :
+4 
+0
+Sample Output 2 :
+0
+*/
+
+int multiplyNumbers(int m, int n) {
+    if(m==0 || n==0){
+        return 0;
+    }
+    else
+        return n + multiplyNumbers(m-1, n);
+}
--- Multiplication (Recursive).cpp
+++
@@ -1,7 +0,0 @@
-int multiplyNumbers(int m, int n) {
-    if(m==0 || n==0){
-        return 0;
-    }
-    else
-        return n + multiplyNumbers(m-1, n);
-}
