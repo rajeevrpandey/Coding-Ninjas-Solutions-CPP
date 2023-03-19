479b02f87e98b963e1f685b4976d8588c77f0240
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:19:25 +0530

    Update and rename Check Palindrome (recursive).cpp to Lecture 3: Recursion 1/Check Palindrome (recursive).cpp

--- Lecture 3: Recursion 1/Check Palindrome (recursive).cpp
+++ Lecture 3: Recursion 1/Check Palindrome (recursive).cpp
@@ -1,3 +1,22 @@
+/*
+Check whether a given String S is a palindrome using recursion. Return true or false.
+Input Format :
+String S
+Output Format :
+'true' or 'false'
+Constraints :
+0 <= |S| <= 1000
+where |S| represents length of string S.
+Sample Input 1 :
+racecar
+Sample Output 1:
+true
+Sample Input 2 :
+ninja
+Sample Output 2:
+false
+*/
+
 bool checkPalindrome(char input[]) {
     int i = 0;
     while (input[i])
