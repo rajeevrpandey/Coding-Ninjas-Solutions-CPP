d07bf61debaee368bcd1dc4b56e7317b9a4e1a9b
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:06:06 +0530

    Update Minimum Length Word.cpp

--- Test 2 (Milestone 2)/Minimum Length Word.cpp
+++ Test 2 (Milestone 2)/Minimum Length Word.cpp
@@ -1,3 +1,23 @@
+/*
+Given a string S (that can contain multiple words), you need to find the word which has minimum length.
+Note : If multiple words are of same length, then answer will be first minimum length word in the string.
+Words are seperated by single space only.
+Input Format :
+String S
+Output Format :
+Minimum length word
+Constraints :
+1 <= Length of String S <= 10^5
+Sample Input 1 :
+this is test string
+Sample Output 1 :
+is
+Sample Input 2 :
+abc de ghihjk a uvw h j
+Sample Output 2 :
+a
+*/
+
 /* input - Input String
 *  output - Save the result in the output array (passed as argument). You don’t have to 
 *  print or return the result
