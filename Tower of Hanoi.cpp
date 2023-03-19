e6d06c8c717758b8244ecb3a77f6a6461c65efd4
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:24:49 +0530

    Update and rename Tower of Hanoi.cpp to Assignment: Recursion 1b/Tower of Hanoi.cpp

--- Tower of Hanoi.cpp
+++
@@ -1,9 +0,0 @@
-void towerOfHanoi(int n, char source, char auxiliary, char destination) {
-    if(n<1)
-        return;
-    else{
-        towerOfHanoi(n-1, source, destination, auxiliary);
-        cout << source << ' ' << destination << endl;
-        towerOfHanoi(n-1, auxiliary, source, destination);
-    }
-}
