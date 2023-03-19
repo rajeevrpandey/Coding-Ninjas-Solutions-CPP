83b337b33266bbaa428b463cb95306109496aebb
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:54:47 +0530

    Update and rename Array Sum.cpp to Lecture 8: Arrays/Array Sum.cpp

--- Lecture 8: Arrays/Array Sum.cpp
+++ Lecture 8: Arrays/Array Sum.cpp
@@ -1,3 +1,18 @@
+/*
+Given an array of length N, you need to find and print the sum of all elements of the array.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Output Format :
+Sum
+Constraints :
+1 <= N <= 10^6
+Sample Input :
+3
+9 8 9
+Sample Output :
+26
+*/
 #include<iostream>
 #include<climits>
 using namespace std;
