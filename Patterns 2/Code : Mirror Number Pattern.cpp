df8cd73604052b932b915b468003d65127e59a47
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 08:04:14 +0530

    Update and rename Code : Mirror Number Pattern.cpp to Patterns 2/Code : Mirror Number Pattern.cpp

--- Patterns 2/Code : Mirror Number Pattern.cpp
+++ Patterns 2/Code : Mirror Number Pattern.cpp
@@ -1,3 +1,31 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+   1 
+  12
+ 123
+1234
+
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+3
+Sample Output 1:
+    1 
+   12
+  123
+Sample Input 2:
+4
+Sample Output 2:
+   1 
+  12
+ 123
+1234
+*/
 #include<iostream>
 using namespace std;
 
