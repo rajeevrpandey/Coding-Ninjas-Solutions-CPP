6d7c382fdcf69e3a4e5c8f0d551730a6460d53da
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 08:08:46 +0530

    Update and rename Code : Diamond of stars.cpp to Patterns 2/Code : Diamond of stars.cpp

--- Patterns 2/Code : Diamond of stars.cpp
+++ Patterns 2/Code : Diamond of stars.cpp
@@ -1,3 +1,37 @@
+/*
+Print the following pattern for the given number of rows.
+Note: N is always odd.
+
+
+Pattern for N = 5
+
+  *
+ ***
+*****
+ ***
+  *
+  
+Input format :
+N (Total no. of rows and can only be odd)
+Output format :
+Pattern in N lines
+Constraints :
+1 <= N <= 49
+Sample Input 1:
+5
+Sample Output 1:
+  *
+ ***
+*****
+ ***
+  *
+Sample Input 2:
+3
+Sample Output 2:
+  *
+ ***
+  *
+*/
 #include<iostream>
 using namespace std;
 
