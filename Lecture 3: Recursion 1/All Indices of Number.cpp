c53c3cbcf0496c843e80e0151de041eb085752bd
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:16:20 +0530

    Update and rename All Indices of Number.cpp to Lecture 3: Recursion 1/All Indices of Number.cpp

--- Lecture 3: Recursion 1/All Indices of Number.cpp
+++ Lecture 3: Recursion 1/All Indices of Number.cpp
@@ -1,3 +1,22 @@
+/*
+Given an array of length N and an integer x, you need to find all the indexes where x is present in the input array. Save all the indexes in the output array (in increasing order).
+Do this recursively. Indexing in the array starts from 0.
+Input Format :
+Line 1 : An Integer N i.e. size of array
+Line 2 : N integers which are elements of the array, separated by spaces
+Line 3 : Integer x
+Output Format :
+Return the size of the output array
+Constraints :
+1 <= N <= 10^3
+Sample Input :
+5
+9 8 10 8 8
+8
+Sample Output :
+1 3 4
+*/
+
 int allIndexes(int input[], int size, int x, int output[]) {
   /* Don't write main().
      Don't read input, it is passed as function argument.
