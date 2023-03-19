58602cc527ec311f00613ab269cc39b0b5313210
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:47:56 +0530

    Update and rename Pyramid Number Pattern.cpp to Test 1/Pyramid Number Pattern.cpp

--- Test 1/Pyramid Number Pattern.cpp
+++ Test 1/Pyramid Number Pattern.cpp
@@ -1,3 +1,23 @@
+/*
+Print the following pattern for the given number of rows.
+Pattern for N = 4
+   1
+  212
+ 32123
+4321234
+Input format : N (Total no. of rows)
+
+Output format : Pattern in N lines
+
+Sample Input :
+5
+Sample Output :
+    1
+   212
+  32123
+ 4321234
+543212345
+*/
 #include<iostream>
 using namespace std;
 
