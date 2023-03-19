f980db8fdf1e0ccc230fb387c2b5349ab693918a
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:34:29 +0530

    Update and rename All Prime Numbers.cpp to Operators and For Loop/All Prime Numbers.cpp

--- Operators and For Loop/All Prime Numbers.cpp
+++ Operators and For Loop/All Prime Numbers.cpp
@@ -1,3 +1,31 @@
+/*
+Given an integer N, print all the prime numbers that lie in the range 2 to N (both inclusive).
+Print the prime numbers in different lines.
+Input Format :
+Integer N
+Output Format :
+Prime numbers in different lines
+Constraints :
+1 <= N <= 100
+Sample Input 1:
+9
+Sample Output 1:
+2
+3
+5
+7
+Sample Input 2:
+20
+Sample Output 2:
+2
+3
+5
+7
+11
+13
+17
+19
+*/
 #include <iostream>
 using namespace std;
 
