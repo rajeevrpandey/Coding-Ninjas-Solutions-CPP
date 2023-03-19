a6d060be99ffcd17f3bfe762aef89ffe595d4c3d
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 07:37:24 +0530

    Update Check Case.cpp

--- Conditionals and Loops/Check Case.cpp
+++ Conditionals and Loops/Check Case.cpp
@@ -1,3 +1,27 @@
+/*
+Write a program that takes a character as input and prints either 1, 0 or -1 according to the following rules.
+1, if the character is an uppercase alphabet (A - Z)
+0, if the character is a lowercase alphabet (a - z)
+-1, if the character is not an alphabet
+Input format :
+Single Character
+Output format :
+1 or 0 or -1
+Constraints :
+Input can be any character
+Sample Input 1 :
+v
+Sample Output 1 :
+0
+Sample Input 2 :
+V
+Sample Output 2 :
+1
+Sample Input 3 :
+#
+Sample Output 3 :
+-1
+*/
 #include<iostream>
 using namespace std;
 
