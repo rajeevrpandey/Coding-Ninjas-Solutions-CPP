4eea149b0e154d632d6841e3654591bd7f9ad8a5
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 14:52:02 +0530

    Update and rename Print All Substrings.cpp to Lecture 10: Character Arrays and 2D Arrays/Print All Substrings.cpp

---
+++ Lecture 10: Character Arrays and 2D Arrays/Print All Substrings.cpp
@@ -0,0 +1,56 @@
+/*
+For a given input string(str), write a function to print all the possible substrings.
+Substring
+A substring is a contiguous sequence of characters within a string. 
+Example: "cod" is a substring of "coding". Whereas, "cdng" is not as the characters taken are not contiguous
+Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+Output Format:
+Print the total number of substrings possible, where every substring is printed on a single line and hence the total number of output lines will be equal to the total number of substrings.
+Note:
+The order in which the substrings are printed, does not matter.
+Constraints:
+0 <= N <= 10^6
+Where N is the length of the input string.
+
+Time Limit: 1 second
+Sample Input 1:
+abc
+Sample Output 1:
+a 
+ab 
+abc 
+b 
+bc 
+c 
+ Sample Input 2:
+co
+Sample Output 2:
+c 
+co 
+o
+*/
+void printSubstrings(char input[]) {
+    // outermost for loop
+    // this is for the selection
+    // of starting point
+   	for (int i = 0; input[i]; i++) {
+       
+        // 2nd for loop is for selection
+        // of ending point
+        for (int j = i; input[j]; j++) {
+           
+            // 3rd loop is for printing from
+            // starting point to ending point
+            for (int k = i; k <= j; k++) {
+                cout << input[k];
+            }
+           
+            // changing the line after printing
+            // from starting point to ending point
+            cout << endl;
+        }
+    }
+ 
+    
+}
