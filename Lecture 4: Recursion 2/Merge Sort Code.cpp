eca9f6dfdae16a00f3951e8e2d56cb4605ae2100
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:28:28 +0530

    Update and rename Merge Sort Code.cpp to Lecture 4: Recursion 2/Merge Sort Code.cpp

--- Lecture 4: Recursion 2/Merge Sort Code.cpp
+++ Lecture 4: Recursion 2/Merge Sort Code.cpp
@@ -1,3 +1,25 @@
+/*
+Sort an array A using Merge Sort.
+Change in the input array itself. So no need to return or print anything.
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 10^3
+Sample Input 1 :
+6 
+2 6 8 5 4 3
+Sample Output 1 :
+2 3 4 5 6 8
+Sample Input 2 :
+5
+2 1 5 2 3
+Sample Output 2 :
+1 2 2 3 5 
+*/
+
 #include<climits>
 
 
