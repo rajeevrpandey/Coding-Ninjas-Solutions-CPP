81d30a813ad20c9e559054e668f7e8917cd55c0d
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:05:25 +0530

    Update Print 2D Array.cpp

--- Test 2 (Milestone 2)/Print 2D Array.cpp
+++ Test 2 (Milestone 2)/Print 2D Array.cpp
@@ -1,3 +1,24 @@
+/*
+Given a 2D integer array with n rows and m columns. Print the 0th row from input n times, 1st row n-1 times…..(n-1)th row will be printed 1 time.
+Input format :
+Line 1 : No of rows (n) and no of columns (m) (separated by single space)
+Line 2 : Row 1 elements (separated by space)
+Line 3 : Row 2 elements (separated by space)
+Line 4 : and so on
+Sample Input 1:
+3 3
+1 2 3
+4 5 6
+7 8 9
+Sample Output 1 :
+1 2 3
+1 2 3
+1 2 3
+4 5 6
+4 5 6
+7 8 9
+*/
+
 #include <iostream>
 using namespace std;
 
