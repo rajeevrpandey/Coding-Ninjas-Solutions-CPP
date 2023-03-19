22d65f7c0010b035a174d6463d566c8d275e55dd
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:21:39 +0530

    Update and rename Replace pi (recursive).cpp to Assignment: Recursion 1b/Replace pi (recursive).cpp

--- Assignment: Recursion 1b/Replace pi (recursive).cpp
+++ Assignment: Recursion 1b/Replace pi (recursive).cpp
@@ -1,3 +1,24 @@
+/*
+Given a string, compute recursively a new string where all appearances of "pi" have been replaced by "3.14".
+Constraints :
+1 <= |S| <= 50
+where |S| represents the length of string S. 
+Sample Input 1 :
+xpix
+Sample Output :
+x3.14x
+Sample Input 2 :
+pipi
+Sample Output :
+3.143.14
+Sample Input 3 :
+pip
+Sample Output :
+3.14p
+Constraints:-
+1<=|S|<=50
+*/
+
 // Change in the given string itself. So no need to return or print anything
 
 void replacePi(char input[]) {
