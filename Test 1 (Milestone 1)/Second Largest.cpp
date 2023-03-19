dfcbbe501a0b2cddae82665a5a1ebb52ff5b3e87
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:51:01 +0530

    Update Second Largest.cpp

--- Test 1 (Milestone 1)/Second Largest.cpp
+++ Test 1 (Milestone 1)/Second Largest.cpp
@@ -1,3 +1,25 @@
+/*
+Take input a stream of n integer elements, find the second largest element present.
+The given elements can contain duplicate elements as well. If only 0 or 1 element is given, the second largest should be INT_MIN ( - 2^31 ).
+Input format :
+
+Line 1 : Total number of elements (n)
+
+Line 2 : N elements (separated by space)
+
+Sample Input 1:
+ 4
+ 3 9 0 9
+Sample Output 1:
+ 3
+Sample Input 2 :
+ 2
+ 4 4
+Sample Output 2:
+ -2147483648
+Sample Output Explanation:
+Since both the elements are equal here, hence second largest element is INT_MIN i.e. ( -2^31 )
+*/
 #include<iostream>
 using namespace std;
 #include <climits>
