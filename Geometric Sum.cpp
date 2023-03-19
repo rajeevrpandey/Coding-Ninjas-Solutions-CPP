1106bf4910380020470dbe50529ea96153d3b860
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:18:45 +0530

    Update and rename Geometric Sum.cpp to Lecture 3: Recursion 1/Geometric Sum.cpp

--- Geometric Sum.cpp
+++
@@ -1,7 +0,0 @@
-#include <math.h>
-double geometricSum(int k) {
-    if(k==0)
-        return 1;
-    else
-        return pow(2, -k) + geometricSum(k-1);
-}
---
+++ Lecture 3: Recursion 1/Geometric Sum.cpp
@@ -0,0 +1,29 @@
+/*
+Given k, find the geometric sum i.e.
+1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) 
+using recursion.
+Input format :
+Integer k
+Output format :
+Geometric sum (upto 5 decimal places)
+Constraints :
+0 <= k <= 1000
+Sample Input 1 :
+3
+Sample Output 1 :
+1.87500
+Sample Input 2 :
+4
+Sample Output 2 :
+1.93750
+Explanation for Sample Input 1:
+1+ 1/(2^1) + 1/(2^2) + 1/(2^3) = 1.87500
+*/
+
+#include <math.h>
+double geometricSum(int k) {
+    if(k==0)
+        return 1;
+    else
+        return pow(2, -k) + geometricSum(k-1);
+}
