7ce44c38dd29a09746249e35de6944430089a00d
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 07:43:51 +0530

    Update Number Pattern 1.cpp

--- Conditionals and Loops/Number Pattern 1.cpp
+++ Conditionals and Loops/Number Pattern 1.cpp
@@ -1,3 +1,21 @@
+/*
+Print the following pattern
+Pattern for N = 4
+1
+23
+345
+4567
+Input Format :
+N (Total no. of rows)
+Output Format :
+Pattern in N lines
+Sample Input 1 :
+3
+Sample Output 1 :
+1
+23
+345
+*/
 #include<iostream>
 using namespace std;
 
