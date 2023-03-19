4501fc9f88da994acae1cad3cdb7c2dfddf9c1f0
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:50:30 +0530

    Update Number Star Pattern.cpp

--- Test 1 (Milestone 1)/Number Star Pattern.cpp
+++ Test 1 (Milestone 1)/Number Star Pattern.cpp
@@ -1,3 +1,18 @@
+/*
+Print the following pattern for given number of rows.
+Input format :
+
+Line 1 : N (Total number of rows)
+
+Sample Input :
+   5
+Sample Output :
+1234554321
+1234**4321
+123****321
+12******21
+1********1
+*/
 #include<iostream>
 using namespace std;
 
