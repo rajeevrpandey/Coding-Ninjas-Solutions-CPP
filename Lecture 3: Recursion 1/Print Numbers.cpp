db02e5c9e436b079931478d33af9e3425df44a69
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:11:53 +0530

    Update and rename Print Numbers.cpp to Lecture 3: Recursion 1/Print Numbers.cpp

---
+++ Lecture 3: Recursion 1/Print Numbers.cpp
@@ -0,0 +1,25 @@
+/*
+Given the number 'n', write a code to print numbers from 1 to n in increasing order recursively.
+Input Format :
+Integer n
+Output Format :
+Numbers from 1 to n (separated by space)
+Constraints :
+1 <= n <= 10000
+Sample Input 1 :
+ 6
+Sample Output 1 :
+1 2 3 4 5 6
+Sample Input 2 :
+ 4
+Sample Output 2 :
+1 2 3 4
+*/
+
+void print(int n){
+    //write your code here
+    if(n == 0)
+        return;
+    print(n-1);
+    cout << n << " ";
+}
--- Print Numbers.cpp
+++
@@ -1,7 +0,0 @@
-void print(int n){
-    //write your code here
-    if(n == 0)
-        return;
-    print(n-1);
-    cout << n << " ";
-}
