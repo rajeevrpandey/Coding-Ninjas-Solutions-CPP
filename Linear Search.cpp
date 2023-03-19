3b3a7b78c3b33feae9482bc6a9744d37cfa35f97
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 09:55:36 +0530

    Update and rename Linear Search.cpp to Lecture 8: Arrays/Linear Search.cpp

--- Linear Search.cpp
+++
@@ -1,9 +0,0 @@
-int linearSearch(int *arr, int n, int x)
-{
-    //Write your code here
-    	for(int i=0;i<n;i++){
-            if(arr[i] == x)
-                return i;
-        }
-    return -1;
-}
