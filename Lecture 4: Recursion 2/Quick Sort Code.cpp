5a48364f7b368fcc93a505f986144e6e631b3ce8
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 15:29:30 +0530

    Update and rename Quick Sort Code.cpp to Lecture 4: Recursion 2/Quick Sort Code.cpp

--- Lecture 4: Recursion 2/Quick Sort Code.cpp
+++ Lecture 4: Recursion 2/Quick Sort Code.cpp
@@ -1,4 +1,28 @@
 /*
+Sort an array A using Quick Sort.
+Change in the input array itself. So no need to return or print anything.
+
+
+Input format :
+Line 1 : Integer n i.e. Array size
+Line 2 : Array elements (separated by space)
+Output format :
+Array elements in increasing order (separated by space)
+Constraints :
+1 <= n <= 10^3
+Sample Input 1 :
+6 
+2 6 8 5 4 3
+Sample Output 1 :
+2 3 4 5 6 8
+Sample Input 2 :
+5
+1 5 2 7 3
+Sample Output 2 :
+1 2 3 5 7 
+*/
+
+/*
 	To apply the method explained in the hint video, we have to add two more parameters in the function call. This can be done by calling a helper function from given function. The helper function can have three parameters: array, start index, end index.  
 	
 	Skeleton code to achieve this:
