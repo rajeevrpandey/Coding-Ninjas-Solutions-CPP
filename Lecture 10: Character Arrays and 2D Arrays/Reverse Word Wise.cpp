914b59c8f1f032a7c3f0edc16f72759b34b44a28
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 14:50:13 +0530

    Update and rename Reverse Word Wise.cpp to Lecture 10: Character Arrays and 2D Arrays/Reverse Word Wise.cpp

--- Lecture 10: Character Arrays and 2D Arrays/Reverse Word Wise.cpp
+++ Lecture 10: Character Arrays and 2D Arrays/Reverse Word Wise.cpp
@@ -1,3 +1,22 @@
+/*
+Reverse the given string word wise. That is, the last word in given string should come at 1st place, last second word at 2nd place and so on. 
+Individual words should remain as it is.
+Input format :
+String in a single line
+Output format :
+Word wise reversed string in a single line
+Constraints :
+0 <= |S| <= 10^7
+where |S| represents the length of string, S.
+Sample Input 1:
+Welcome to Coding Ninjas
+Sample Output 1:
+Ninjas Coding to Welcome
+Sample Input 2:
+Always indent your code
+Sample Output 2:
+code your indent Always
+*/
 void reverseStringWordWise(char input[]) {
     // Write your code here
     int len = 0;
