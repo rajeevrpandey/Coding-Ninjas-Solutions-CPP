a844802c1b30b0ad461bce58d06a9fac2dc0f1c9
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 14:48:18 +0530

    Update and rename Trim Spaces.cpp to Lecture 10: Character Arrays and 2D Arrays/Trim Spaces.cpp

--- Lecture 10: Character Arrays and 2D Arrays/Trim Spaces.cpp
+++ Lecture 10: Character Arrays and 2D Arrays/Trim Spaces.cpp
@@ -1,3 +1,17 @@
+/*
+Given an input string S that contains multiple words, you need to remove all the spaces present in the input string.
+There can be multiple spaces present after any word.
+Input Format :
+ String S
+Output Format :
+Updated string
+Constraints :
+1 <= Length of string S <= 10^6
+Sample Input :
+abc def g hi
+Sample Output :
+abcdefghi
+*/
 void trimSpaces(char input[]) {
     // Write your code here
     int i = 0,j=0;
