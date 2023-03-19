c49977ec6c86f7dc552cd0f35c264d28e1ce91c1
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 07:39:01 +0530

    Update and rename Sum of Even Numbers till N.cpp to Conditionals and Loops/Sum of Even Numbers till N.cpp

--- Conditionals and Loops/Sum of Even Numbers till N.cpp
+++ Conditionals and Loops/Sum of Even Numbers till N.cpp
@@ -1,4 +1,14 @@
 /*
+Given a number N, print sum of all even numbers from 1 to N.
+Input Format :
+Integer N
+Output Format :
+Required Sum 
+Sample Input 1 :
+ 6
+Sample Output 1 :
+12
+
 	Pre-requisite to solve this problem:
 	In this question, we have to find sum of all even numbers till n. To take sum of all the even numbers, we have to take a variable and initialize it to zero. Let us name this variable as sum. 
 	Following code is used to add 2 to sum variable and update it to the same sum variable:
