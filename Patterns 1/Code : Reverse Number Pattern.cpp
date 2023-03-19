392ff7a537eb7f6d4b295ed2523906c46a749ecf
RAJEEV RANJAN PANDEY <40412213+rajeevrpandey@users.noreply.github.com>
2023-03-19 07:58:45 +0530

    Update and rename Code : Reverse Number Pattern.cpp to Patterns 1/Code : Reverse Number Pattern.cpp

---
+++ Patterns 1/Code : Reverse Number Pattern.cpp
@@ -0,0 +1,48 @@
+/*
+Print the following pattern for the given N number of rows.
+Pattern for N = 4
+1
+21
+321
+4321
+Input format :
+Integer N (Total no. of rows)
+Output format :
+Pattern in N lines
+Constraints
+0 <= N <= 50
+Sample Input 1:
+5
+Sample Output 1:
+1
+21
+321
+4321
+54321
+Sample Input 2:
+6
+Sample Output 2:
+1
+21
+321
+4321
+54321
+654321
+*/
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+       /*  Read input as specified in the question.
+	* Print output as specified in the question.
+	*/int n;
+    cin >> n;
+    for(int i = 1; i<=n;i++){
+        for(int j = 1; j<=i;j++)
+            cout << i-j+1;
+        cout << endl;
+    }
+  
+}
